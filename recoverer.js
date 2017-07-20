c3cb65a97ff99905643bd56128ece87e38e58b5c
Dan Finlay <dan@danfinlay.com>
2017-07-19 18:14:19 -0700

    Simplified

--- recoverer.js
+++ recoverer.js
@@ -2,9 +2,8 @@ var ethUtil = require('ethereumjs-util')
 
 module.exports = function (msg, sigHex) {
 
-  const msgHash = ethUtil.sha3(new Buffer(msg))
+  const msgHash = ethUtil.sha3(msg)
   const sigParams = ethUtil.fromRpcSig(sigHex)
-  console.dir({ v: sigParams.v })
   const publicKey = ethUtil.ecrecover(msgHash, sigParams.v, sigParams.r, sigParams.s)
 
   const sender = ethUtil.bufferToHex(ethUtil.publicToAddress(publicKey))
